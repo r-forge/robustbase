a3eaa22a9238f8d485be28f90877bf5fed33ee74
econceicao (Eduardo Conceição) <mail@eduardoconceicao.org>
2023-07-09 03:30:49 +0000

    NCDEoptim-tst.R: add constrained test
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/robustbase@944 694ef91d-65df-0310-b7bb-92e67a308ead

--- pkg/DEoptimR/tests/NCDEoptim-tst.R
+++ pkg/DEoptimR/tests/NCDEoptim-tst.R
@@ -22,13 +22,37 @@ bl <- function(x) {
     sum((abs(x) - 5)^2)
 }
 
-S.time(bl_ <- NCDEoptim(-c(10, 10), c(10, 10), bl,
-                        niche_radius = 5, maxiter = 100))
+S.time(bl_ <- NCDEoptim(-c(10, 10), c(10, 10),
+                        bl,
+                        niche_radius = 5,
+                        maxiter = 100))
+# Only inequality constraints --------------------------------------------------
+
+#   Function F1
+#
+#   f(x) = x^2
+#   subject to:
+#   g(x) = 1 - x^2 <= 0
+#
+#   -2 <= x <= 2
+#   The two global optima are (x1*, x2*; f*) = (1, -1; 1).
+#
+#   Source:
+#     Poole, Daniel J. and Allen, Christian B. (2019).
+#     Constrained niching using differential evolution.
+#     Swarm and Evolutionary Computation 44, 74-100.
+
+S.time(F1_ <- NCDEoptim(-2, 2,
+                        \(x) x^2,
+                        \(x) 1 - x^2,
+                        niche_radius = 1,
+                        maxiter = 200))
 
 # Expected optimal values ------------------------------------------------------
 
 stopifnot(
-  all.equal( as.vector(abs(bl_$solution_arch)), rep(5, 8), tolerance = 1e-4 )
+  all.equal( as.vector(abs(bl_$solution_arch)), rep(5, 8), tolerance = 1e-3 ),
+  all.equal( as.vector(abs(F1_$solution_arch)), c(1, 1), tolerance = 1e-2 )
 )
 
 c.time(proc.time())
