3d5fa0ab38f59d4a9a301fde4ef65afaa40664d8
econceicao (Eduardo Conceição) <mail@eduardoconceicao.org>
2026-08-11 21:18:55 +0000

    Also for NCDEoptim()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/robustbase@1028 694ef91d-65df-0310-b7bb-92e67a308ead

--- pkg/DEoptimR/R/NCDEoptim.R
+++ pkg/DEoptimR/R/NCDEoptim.R
@@ -395,6 +395,7 @@ NCDEoptim <- function(
         stopifnot(is.matrix(hpop) || is.vector(hpop),
                   !anyNA(hpop), !is.nan(hpop), !is.logical(hpop))
         if (is.vector(hpop)) dim(hpop) <- c(1, length(hpop))
+        stopifnot(nrow(hpop) >= meq)
         TAVpop <- apply( hpop, 2, function(x) sum(pmax(x, 0)) )
         mu <- median(TAVpop)
         hpop_next <- hpop
